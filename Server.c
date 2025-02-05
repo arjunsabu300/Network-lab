b127e0417556bcdba793e9fecd635d8de1610425
Arjun Sabu <110405633+arjunsabu300@users.noreply.github.com>
2025-02-05 15:35:21 +0530

    Update Server.c

--- Server.c
+++ Server.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void main()
-{
-   int port=8000;
-   struct sockaddr_in serv_addr,cli_addr;
-   socklen_t clilen;
-   char buffer[255];
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-   bzero((char*)&serv_addr,sizeof(serv_addr));
-   serv_addr.sin_family=AF_INET;
-   serv_addr.sin_addr.s_addr=INADDR_ANY;
-   serv_addr.sin_port=htons(port);
+int main() {
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
 
-   int sockfd=socket(AF_INET,SOCK_STREAM,0);
-   if(sockfd <0)
-      printf("Error opening socket\n");
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-   bind(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
-   listen(sockfd,5);
-   clilen=sizeof(cli_addr);
-   int newsockfd=accept(sockfd,(struct sockaddr*)&cli_addr,&clilen);
+    bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
 
-   while(1)
-   {
-      bzero(buffer,255);
-      read(newsockfd,buffer,25);
-      printf("Client: %s\n",buffer);
-      bzero(buffer,255);
-      fgets(buffer,255,stdin);
-      write(newsockfd,buffer,strlen(buffer));
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
+        sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *)&client_addr, client_len);
+    }
 
-      if(strncasecmp(buffer,"exit",4) ==0)
-         break;
-   }
-   close(newsockfd);
-   close(sockfd);
+    close(sockfd);
+    return 0;
 }
