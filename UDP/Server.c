1dbc658ec2a1b8402a8bc577f99413387d3e1e38
Arjun Sabu <110405633+arjunsabu300@users.noreply.github.com>
2025-02-13 11:06:39 +0530

    Update Server.c

--- UDP/Server.c
+++ UDP/Server.c
@@ -1,32 +1,64 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+int main()
+{
+        int serversocket;
+        char buffer[1024];
+        struct sockaddr_in serverAddr,clientaddr;
+        socklen_t addrsize;
 
-#define PORT 8080
-#define BUFFER_SIZE 1024
+        serversocket=socket(AF_INET,SOCK_DGRAM,0);
+        if(serversocket<0)
+        {
+                perror("Socket creation failed");
+                return -1;
+        }
+        serverAddr.sin_family=AF_INET;
+        serverAddr.sin_port=htons(6067);
+        serverAddr.sin_addr.s_addr=inet_addr("127.0.0.75");
 
-int main() {
-    int sockfd;
-    char buffer[BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+        if(bind(serversocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr))<0)
+        {
+                perror("Bind failed");
+                close(serversocket);
+                return -1;
+        }
+        printf("server is listening...\n");
+        addrsize=sizeof(clientaddr);
+        while(1)
+        {
+                memset(buffer,0,sizeof(buffer));
 
-    bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+                int recvLen=recvfrom(serversocket,buffer,sizeof(buffer)-1,0,(struct sockaddr*)&clientaddr,&addrsize);
+                if(recvLen>0)
+                {
+                        buffer[recvLen]='\0';
+                        printf("Client:%s\n",buffer);
+                        if(strcmp(buffer,"exit")==0)
+                        {
+                                printf("Client exited.\n");
+                                continue;
+                        }
+                        memset(buffer,0,sizeof(buffer));
+                        printf("Enter message : ");
+                        fgets(buffer,sizeof(buffer),stdin);
+                        buffer[strcspn(buffer,"\n")]='\0';
+                        sendto(serversocket,buffer,strlen(buffer),0,(struct sockaddr*)&clientaddr,addrsize);
+                        if(strcmp(buffer,"exit")==0)
+                        {
+                                printf("Server Shutting down...\n");
+                                exit(1);
+                        }
 
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
-        sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *)&client_addr, client_len);
-    }
-
-    close(sockfd);
-    return 0;
+                }
+        }
+        close(serversocket);
+        return 0;
 }
+
